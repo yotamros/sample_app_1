56794628e71d18993da2cccc28cfe2e37209d66d
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-06-16 16:20:33 -0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,21 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   before_save { email.downcase! }
+  before_create :create_remember_token
   validates :password, length: { minimum: 6 }
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  private
+  
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,21 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
   
   it { should be_valid }
   
@@ -107,5 +122,12 @@ describe User do
         specify { expect(user_for_invalid_password).to be_false }
       end 
   end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
+  
   
 end
